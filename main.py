d13c35a4cf9a0aa25fa678e60c2cba5d64e9291f
Stephegbenga <adegbamiyestephen2018@gmail.com>
2022-08-14 12:58:24 +0100

    update

--- main.py
+++ main.py
@@ -7,15 +7,11 @@ api = Api(app)
 cron_jobs = getall()
 if cron_jobs:
     for cron_job in cron_jobs:
-        try:
-            date = cron_job["date"]
-            id = cron_job["id"]
-            data = process(date, id)
-            if data['status'] == 'error':
-                remove_from_list({"id": id, "date":date})
-        except Exception as e:
-            print(e)
-
+        date = cron_job["date"]
+        id = cron_job["id"]
+        data = process(date, id)
+        if data['status'] == 'error':
+            remove_from_list({"id": id, "date":date})
 
 
 @app.route('/')
